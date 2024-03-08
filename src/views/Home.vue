d9540b51e760c41b68b4d61e9fd9121c29e7e67b
LuckyNoobz <r.abudzar@gmail.com>
2024-03-08 21:41:13 +0700

    first setup

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <p>Hi, I'm {{name}}, heres my number {{number}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  setup() {
+    const name = "Alghi";
+    const number = "08713243184";
+
+    return { name: name, number: number}
+  },
+};
+</script>
