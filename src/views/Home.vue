dc6e0f52480587bc6742e1dc7b073bb3e9d4030e
LuckyNoobz <r.abudzar@gmail.com>
2024-03-09 13:43:57 +0700

    make data on API Composition

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,25 @@
 <template>
   <div class="home">
-    <p>Hi, I'm {{name}}, heres my number {{number}}</p>
+    <p ref="p">Hi, I'm {{ name }}, heres my number {{ number }}</p>
+    <button @click="handleClick">Click!</button>
   </div>
 </template>
 
 <script>
+import { ref } from "vue";
 export default {
   name: "Home",
   setup() {
+    const p = ref(null);
     const name = "Alghi";
     const number = "08713243184";
 
-    return { name: name, number: number}
+    const handleClick = () => {
+      console.log(p, p.value);
+      p.value.classList.add("paragraph");
+      p.value.textContent = `hi, alghi!`
+    };
+    return { name, number, p, handleClick };
   },
 };
 </script>
